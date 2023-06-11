f6ee439f2d7a4da514baa99af37a5197414af957
Saakshi Adiga <f20201930@goa.bits-pilani.ac.in>
2023-06-11 11:40:50 +0530

    changes

---
+++ lib/pages/prevRequests.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PrevRequests extends StatefulWidget {
+  const PrevRequests({super.key});
+
+  @override
+  State<PrevRequests> createState() => _PrevRequestsState();
+}
+
+class _PrevRequestsState extends State<PrevRequests> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+      ),
+    );
+  }
+}
